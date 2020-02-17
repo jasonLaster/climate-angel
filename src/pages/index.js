a3de99ed493e705b28c7a0f2a88cd439d6fcb632
Jason Laster <jlaster@mozilla.com>
2020-02-17 11:10:20 -0800

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <iframe class="airtable-embed" src="https://airtable.com/embed/shrBwP0cNkVmHQQtK?backgroundColor=pink&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="1800"></iframe>
+  </Layout>
+)
+
+export default IndexPage
